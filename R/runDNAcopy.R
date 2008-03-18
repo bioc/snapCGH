26b2321ea4766fd6aa52bf161ad5dc6d8d2aceee
John Marioni <jcm68@cam.ac.uk>
2008-03-18 21:32:33 +0000

    changes to runDNAcopy function and vignette
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/snapCGH@30568 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/runDNAcopy.R
+++ R/runDNAcopy.R
@@ -1,6 +1,6 @@
 runDNAcopy <- function(input, smooth.region=2, outlier.SD.scale = 4, smooth.SD.scale = 2, trim=0.025, alpha = 0.01, p.method = c("hybrid", 
     "perm"), kmax = 25, nmin = 200, undo.splits = c("none", "prune", "sdundo"), 
-    undo.prune = 0.05, undo.SD = 3, nperm=10000, eta=0.05, sbdry=NULL) {
+    undo.prune = 0.05, undo.SD = 3, nperm=10000, eta=0.05) {
 
   if(length(which(search() == "package:tilingArray")) == 1){
     detach("package:tilingArray")
@@ -18,8 +18,8 @@ runDNAcopy <- function(input, smooth.region=2, outlier.SD.scale = 4, smooth.SD.s
 
   cna <- CNA(log2ratios(input), input$genes$Chr, input$genes$Position, sampleid = colnames(input$M.observed))
   cna <- smooth.CNA(cna, smooth.region=smooth.region, outlier.SD.scale = outlier.SD.scale, smooth.SD.scale = smooth.SD.scale, trim=trim) 
-  dna <- segment(cna, alpha = alpha, p.method = p.method, kmax = kmax, nmin = nmin, nperm=nperm, eta = eta, sbdry=sbdry,
-                 trim = trim, undo.splits = undo.splits, undo.prune = undo.prune, undo.SD = undo.SD)
+  dna <- segment(cna, alpha = alpha, nperm=nperm, p.method = p.method, kmax = kmax, nmin = nmin,  eta = eta,
+                 trim = trim, undo.splits = undo.splits, undo.prune = undo.prune, undo.SD = undo.SD, verbose = 1)
   #changing the output back to the segmentation.info object
 
   template <- matrix(NA, nrow(log2ratios(input)), ncol(log2ratios(input)), dimnames = dimnames(log2ratios(input)))
